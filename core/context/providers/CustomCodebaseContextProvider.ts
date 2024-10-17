c0a982b59ed4e43b5ce5128264e4dcfdf48c2ba4
Rahul Roy <rahul.roy@mindbowser.com>
2024-10-17 11:25:17 +0530

    fix: context is not able to get path

--- core/context/providers/CustomCodebaseContextProvider.ts
+++ core/context/providers/CustomCodebaseContextProvider.ts
@@ -10,24 +10,27 @@ import fsPromises from "fs/promises";
 import os from "os";
 import { getBasename, getUniqueFilePath, groupByLastNPathParts } from "../../util";
 
+const SKIP_ABLE_FOLDERS = ["node_modules"];
+
 async function listFolders(path: string, depth: number = 0): Promise<string[]> {
-  const dir = await fsPromises.readdir(path);
   const folders: string[] = [];
 
-try {
-  for await (const item of dir) {
-    if ((await fsPromises.stat(path + "/" + item)).isDirectory()) {
-      if (depth > 0) {
-        const innerFolders = await listFolders(path + "/" + item, depth - 1);
-        folders.push(...innerFolders);
+  const dir = await fsPromises.readdir(path);
+  try {
+    for await (const item of dir) {
+      if (!item.startsWith(".") && !SKIP_ABLE_FOLDERS.includes(item) && (await fsPromises.stat(path + "/" + item)).isDirectory()) {
+        if (depth > 0) {
+          const innerFolders = await listFolders(path + "/" + item, depth - 1);
+          innerFolders.forEach(f => folders.push(f));
+        }
+        folders.push(path + "/" + item);
       }
-      folders.push(item);
     }
+  } catch (err) {
+    console.error(err);
   }
-} catch (err) {
-  console.error(err);
-}
-return folders;
+
+  return folders;
 }
 
 class CustomCodebaseContextProvider extends BaseContextProvider {
@@ -53,7 +56,7 @@ class CustomCodebaseContextProvider extends BaseContextProvider {
     args: LoadSubmenuItemsArgs,
   ): Promise<ContextSubmenuItem[]> {
     const homeDir = os.homedir();
-    const folders = await listFolders(homeDir, 5);
+    const folders = await listFolders(homeDir, 3);
     const folderGroups = groupByLastNPathParts(folders, 2);
 
     return folders.map((folder) => {
