1214f8a42feaf6b217826d9231394e3a448cb668
Rahul Roy <rahul.roy@mindbowser.com>
2024-11-12 01:42:36 +0530

    fix: recreate config.json on activation

--- extensions/vscode/src/activation/activate.ts
+++ extensions/vscode/src/activation/activate.ts
@@ -1,5 +1,6 @@
-import { getContinueRcPath, getTsConfigPath, migrate } from "core/util/paths";
+import { getContinueGlobalPath, getContinueRcPath, getTsConfigPath, migrate } from "core/util/paths";
 import { Telemetry } from "core/util/posthog";
+import * as fs from "fs";
 import path from "node:path";
 import * as vscode from "vscode";
 import { VsCodeExtension } from "../extension/VsCodeExtension";
@@ -8,8 +9,14 @@ import { getExtensionVersion } from "../util/util";
 import { getExtensionUri } from "../util/vscode";
 import { VsCodeContinueApi } from "./api";
 import { setupInlineTips } from "./inlineTips";
+import { defaultConfig, defaultConfigJetBrains } from "core/config/default";
 
 export async function activateExtension(context: vscode.ExtensionContext) {
+  const p = path.join(getContinueGlobalPath(), "config.json");
+
+  fs.unlink(p, () => 
+    fs.writeFileSync(p, JSON.stringify(defaultConfig, null, 2))
+  );
   // Add necessary files
   getTsConfigPath();
   getContinueRcPath();
