ebf80121e55552535a246368d8b29898a261d720
Rahul Roy <rahul.roy@mindbowser.com>
2024-11-13 00:01:07 +0530

    fix: autoincrement patch for every arch

--- extensions/vscode/scripts/package-all.js
+++ extensions/vscode/scripts/package-all.js
@@ -2,32 +2,32 @@ const { execSync } = require("child_process");
 
 const PLATFORMS = [
   "win32-x64",
-//   "win32-arm64", can't be built due to no sqlite3 binaries
+  //   "win32-arm64", can't be built due to no sqlite3 binaries
   "linux-x64",
   "linux-arm64",
   "darwin-x64",
   "darwin-arm64",
-]
+];
 const args = process.argv.slice(2);
 const isPreRelease = args.includes("--pre-release");
-
+var shouldPatch = true;
 (async () => {
   for (const i in PLATFORMS) {
     const platform = PLATFORMS[i];
     const pkgCommand = isPreRelease
-      ? "node scripts/package.js --pre-release --target " + platform // --yarn"
-      : "node scripts/package.js --target " + platform; // --yarn";
+      ? "node scripts/package.js --pre-release --target " +
+        platform +
+        " --patch " +
+        shouldPatch // --yarn"
+      : "node scripts/package.js --target " +
+        platform +
+        " --patch " +
+        shouldPatch; // --yarn";
 
-    execSync(
-      "node scripts/prepackage-cross-platform.js --target "+ platform,
-      {stdio: 'inherit'}
-
-    );
-    execSync(
-      pkgCommand,
-      {stdio: 'inherit'}
-    );
+    execSync("node scripts/prepackage-cross-platform.js --target " + platform, {
+      stdio: "inherit",
+    });
+    execSync(pkgCommand, { stdio: "inherit" });
+    shouldPatch = false;
   }
 })();
-
-  
